4020999a63a92c24581b787c90bb2009ff1436af
Francisco Hauva <phauva@proton.me>
2025-03-21 17:39:12 -0300

    feat: dashboard empty state

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -43,30 +43,34 @@
 		class="bg-dark-gray-400 border-dark-gray-600 flex w-[600px] flex-col gap-2 rounded border p-4"
 	>
 		<h1 class="mb-4 text-center text-2xl text-white">My URLs</h1>
-		<ul
-			class="flex max-h-[400px] flex-col gap-2 overflow-y-auto [scrollbar-color:#343434_#282828] [scrollbar-width:thin]"
-		>
-			{#each data.urls as url}
-				<li class="bg-dark-gray-600 rounded px-3 py-1 text-white">
-					<p>
-						<button
-							onclick={() => copySlugUrl(url.slug)}
-							class="cursor-pointer text-blue-400 hover:text-blue-300">{url.slug}</button
+		{#if data.urls.length === 0}
+			<p class="text-light-gray-400 text-center text-lg">You haven't created any URLs yet</p>
+		{:else}
+			<ul
+				class="flex max-h-[400px] flex-col gap-2 overflow-y-auto [scrollbar-color:#343434_#282828] [scrollbar-width:thin]"
+			>
+				{#each data.urls as url}
+					<li class="bg-dark-gray-600 rounded px-3 py-1 text-white">
+						<p>
+							<button
+								onclick={() => copySlugUrl(url.slug)}
+								class="cursor-pointer text-blue-400 hover:text-blue-300">{url.slug}</button
+							>
+							• {url.clicks}
+							{pluralizeClicks(url.clicks)} •
+							<span class={url.expiresAt < new Date() ? 'text-red-400' : ''}
+								>{getExpiredText(url.expiresAt)}</span
+							>
+						</p>
+						<!-- TODO: consider doing some overflow wrapping on the url text, it looks kinda ugly -->
+						<a
+							class="text-light-gray-900 hover:text-light-gray-800 [overflow-wrap:anywhere]"
+							href={url.destination}>{url.destination}</a
 						>
-						• {url.clicks}
-						{pluralizeClicks(url.clicks)} •
-						<span class={url.expiresAt < new Date() ? 'text-red-400' : ''}
-							>{getExpiredText(url.expiresAt)}</span
-						>
-					</p>
-					<!-- TODO: consider doing some overflow wrapping on the url text, it looks kinda ugly -->
-					<a
-						class="text-light-gray-900 hover:text-light-gray-800 [overflow-wrap:anywhere]"
-						href={url.destination}>{url.destination}</a
-					>
-				</li>
-			{/each}
-		</ul>
+					</li>
+				{/each}
+			</ul>
+		{/if}
 		<div class="text-center">
 			<div>
 				<a href="/" class="text-light-gray-800 hover:text-light-gray-300 underline">Create a URL</a>
