4e5506540dc72155af8fb5d8220b991d271c453a
Francisco Hauva <phauva@proton.me>
2025-03-21 07:13:24 -0300

    feat: content security policies config

--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,18 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter()
+	},
+	csp: {
+		directives: {
+			'script-src': ['self']
+		},
+		reportOnly: {
+			'script-src': ['self'],
+			'report-uri': ['/']
+		}
 	}
 };
 
